abe1c8bb45238c57356d635a4135bcfe379f758f
RdeJonge <rene@ikbenfrits.nl>
2017-08-14 11:29:55 +0200

    multiple mounts in module

--- app/api/rene/api.rb
+++ app/api/rene/api.rb
@@ -1,5 +1,38 @@
 module Rene
-  class API < Grape::API
+  # class API < Grape::API
+  #   format :json # zonder dit wordt xml gebruikt
+  #   get :test do
+  #     "Rene" # geeft foutmelding bij xml, maar niet bij json   
+  #   end
+
+  #   get :json do
+  #     {
+  #       "1": {
+  #         "1.1": "eerste eerste",
+  #         "1.2": "eerste tweede",
+  #         "1.3": "eerste derde",
+  #         "1.4": [
+  #         {
+  #         "1.4.1a": "eerste vierde a",
+  #         "1.4.1b": "eerste vierde b"
+  #         }],
+  #         "1.5": [
+  #         {
+  #         "1.5.1a": "eerste vijfde eerste a",
+  #         "1.5.1b": "eerste vijfde eerste b"
+  #         },
+  #         {
+  #         "1.5.2a": "eerste vijfde tweede a",
+  #         "1.5.2b": "eerste vijfde tweede b",
+  #         "1.5.2c": "eerste vijfde tweede c"
+  #         }],
+  #         "1.6": "eerste zesde"
+  #       }
+  #     }
+  #   end
+  # end
+
+  class APIv1 < Grape::API
     format :json # zonder dit wordt xml gebruikt
     get :test do
       "Rene" # geeft foutmelding bij xml, maar niet bij json   
@@ -30,5 +63,21 @@ module Rene
         }
       }
     end
+  end
+
+  class APIv2 < Grape::API
+    format :json
+    get :test do
+      "Test Grape Api via test"
+    end
+
+    get :test2 do
+      "Test Grap Api via test2"
+    end
+  end
+
+  class API < Grape::API
+    mount Rene::APIv1
+    mount Rene::APIv2
   end
 end
