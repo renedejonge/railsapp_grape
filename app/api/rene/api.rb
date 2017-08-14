86691cced3ff8691a2cc7fb6a207cfa603ceaca6
RdeJonge <rene@ikbenfrits.nl>
2017-08-14 11:05:04 +0200

    get :json

--- app/api/rene/api.rb
+++ app/api/rene/api.rb
@@ -1,7 +1,34 @@
 module Rene
   class API < Grape::API
+    format :json # zonder dit wordt xml gebruikt
     get :test do
-      "Rene"
+      "Rene" # geeft foutmelding bij xml, maar niet bij json   
+    end
+
+    get :json do
+      {
+        "1": {
+          "1.1": "eerste eerste",
+          "1.2": "eerste tweede",
+          "1.3": "eerste derde",
+          "1.4": [
+          {
+          "1.4.1a": "eerste vierde a",
+          "1.4.1b": "eerste vierde b"
+          }],
+          "1.5": [
+          {
+          "1.5.1a": "eerste vijfde eerste a",
+          "1.5.1b": "eerste vijfde eerste b"
+          },
+          {
+          "1.5.2a": "eerste vijfde tweede a",
+          "1.5.2b": "eerste vijfde tweede b",
+          "1.5.2c": "eerste vijfde tweede c"
+          }],
+          "1.6": "eerste zesde"
+        }
+      }
     end
   end
 end
